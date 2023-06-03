eb9f09feaeae7dcf69741a727364a1f591abcf92
Maciej Kicinski <kicinskimaciej@@gmail.com>
2023-06-03 23:57:01 +0200

    -adding new component Container

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,7 +6,9 @@ const SearchForm = () => {
   return (
     <form className={styles.searchForm}>
       <TextInput placeholder="Search…" />
-      <Button >button</Button>
+      <Button>
+        <span className="fa fa-search" />
+      </Button>
     </form>
   );
 };
