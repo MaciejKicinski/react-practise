c5835e35e912285f5d2924c1fb73023547697eb7
Maciej Kicinski <kicinskimaciej@@gmail.com>
2023-06-29 23:58:59 +0200

    -moving responsbility of creating new column to columnforms, -next step excercise 3

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,6 +1,15 @@
 import styles from './ColumnForm.module.scss';
+import { useState } from "react";
 
-const ColumnForm = () => {
+
+const ColumnForm = props => {
+    const [value, setValue] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({ title: value });
+        setValue('');
+    }
+
 	return (
         <form onSubmit={handleSubmit}>
             <input type="text" value={value} onChange={e => setValue(e.target.value)} />
